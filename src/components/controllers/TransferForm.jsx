b856780dd978395111b0e12ff49f59c5b1462801
DavidSparker <davidsparker0417@gmain.com>
2024-04-22 01:35:50 +0900

    modified:   src/components/controllers/TransferForm.jsx

--- src/components/controllers/TransferForm.jsx
+++ src/components/controllers/TransferForm.jsx
@@ -53,7 +53,7 @@ const TransferForm = () => {
 
     const calculate = (selected, opposite, entered) => {
         fetch(`http://localhost:3000/api/paircode/${selected}/${opposite}/${entered}`)
-        // fetch(`https://api.faremit.com/api/paircode/${selected}/${opposite}/${entered}`)
+            // fetch(`https://api.faremit.com/api/paircode/${selected}/${opposite}/${entered}`)
             .then(res => res.json())
             .then(res => {
                 const rate = res.conversion_rate.toFixed();
